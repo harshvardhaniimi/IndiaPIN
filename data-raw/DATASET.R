f73719d172c4acdeefd356cfc78bf70917ee56d7
harshvardhaniimi <hvsc1708@gmail.com>
2022-01-02 00:01:20 -0500

    creating and loading the dataset

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,6 +1,3 @@
 ## code to prepare IndiaPIN dataset goes here
 
-IndiaPIN <- utils::read.csv(here::here("data-raw", "pincode.csv"))
-IndiaPIN <- tibble::as_tibble(IndiaPIN)
-
 usethis::use_data(IndiaPIN, overwrite = TRUE)
---
+++ data-raw/IndiaPIN_load.R
@@ -0,0 +1,20 @@
+# IndiaPIN_load.R
+# This script is not run when executing the package. It is only used to create the dataset.
+
+# Since some PIN codes have more than one offices, I will only consider the first office. 
+# The goal here is to have each row as an identifier, each row corresponding to one single PIN code.
+
+library(tidyverse)
+
+IndiaPIN = utils::read.csv(here::here("data-raw", "pincode.csv")) %>% 
+  as_tibble() %>% 
+  group_by(Pincode) %>% 
+  filter(row_number() == 1) %>% 
+  rename(Circle = CircleName, Region = RegionName, 
+         Division = DivisionName, Office = OfficeName, 
+         PIN = Pincode, State = StateName) %>% 
+  select(-OfficeType, -Delivery) %>% 
+  mutate(Latitude = as.numeric(Latitude),
+         Longitude = as.numeric(Longitude))
+
+usethis::use_data(IndiaPIN, overwrite = TRUE)
