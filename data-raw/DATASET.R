d6b075d722541e61e8e84918e3559f5278f9fd8f
harshvardhaniimi <hvsc1708@gmail.com>
2022-01-01 23:28:21 -0500

    to tibble

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,5 +1,6 @@
 ## code to prepare IndiaPIN dataset goes here
 
 IndiaPIN <- utils::read.csv(here::here("data-raw", "pincode.csv"))
+IndiaPIN <- tibble::as_tibble(IndiaPIN)
 
 usethis::use_data(IndiaPIN, overwrite = TRUE)
