bfda11f5d5a32a635f2f484450e35405ad8ade17
harshvardhaniimi <hvsc1708@gmail.com>
2022-01-02 00:29:08 -0500

    updaed comments

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,4 @@
-## code to prepare IndiaPIN dataset goes here
+## This script contains codes used to create the package.
+## Follows: https://rstudio4edu.github.io/rstudio4edu-book/data-pkg.html
 
 usethis::use_data(IndiaPIN, overwrite = TRUE)
